4847f692f7b38895e4fa05fbbc241d5dc5f5f071
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-07-14 00:06:29 +0200

    get next feeds, prepend nulls first

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -86,7 +86,7 @@ func (q *Queries) GetFeeds(ctx context.Context) ([]Feed, error) {
 }
 
 const getNextFeedsToFetch = `-- name: GetNextFeedsToFetch :many
-SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds ORDER BY last_fetched_at LIMIT $1
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds ORDER BY last_fetched_at NULLS FIRST LIMIT $1
 `
 
 func (q *Queries) GetNextFeedsToFetch(ctx context.Context, limit int32) ([]Feed, error) {
